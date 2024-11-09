87736e5414af1a4dd7c4238ae3349ba950c315f6
ethanholen-hpe <ethan.holen@hpe.com>
2024-11-09 08:07:48 -0700

    Its a bit of a mess but we r merging this anyway

--- initial_testing/curl_calls.sh
+++ initial_testing/curl_calls.sh
@@ -1,20 +1,21 @@
 
 
-number_of_entries_to_fetch=30
+number_of_months_to_fetch=12
 
 month_to_fetch=10
 day_to_fetch=27
-year_to_fetch=2024
+year_to_fetch=2025
 
 
 
 
-for ((i=1; i<=number_of_entries_to_fetch; i++)); do
-    # echo "$i"
-    curl -s https://orthocal.info/api/gregorian/$year_to_fetch/$month_to_fetch/$i/ | jq '.weekday'
-    curl -s https://orthocal.info/api/gregorian/$year_to_fetch/$month_to_fetch/$i/ | jq '.fast_level_desc'
-    curl -s https://orthocal.info/api/gregorian/$year_to_fetch/$month_to_fetch/$i/ | jq '.feast_level_description'
-    curl -s https://orthocal.info/api/gregorian/$year_to_fetch/$month_to_fetch/$i/ | jq '.fast_level'
-    curl -s https://orthocal.info/api/gregorian/$year_to_fetch/$month_to_fetch/$i/ | jq '.titles' | jq '.[]'
+for ((i=1; i<=number_of_months_to_fetch; i++)); do
+    echo "$i"
+    curl -s https://orthocal.info/api/gregorian/$year_to_fetch/$i/ > $i.json
+    # curl -s https://orthocal.info/api/gregorian/$year_to_fetch/$month_to_fetch/$i/ | jq '.weekday'
+    # curl -s https://orthocal.info/api/gregorian/$year_to_fetch/$month_to_fetch/$i/ | jq '.fast_level_desc'
+    # curl -s https://orthocal.info/api/gregorian/$year_to_fetch/$month_to_fetch/$i/ | jq '.feast_level_description'
+    # curl -s https://orthocal.info/api/gregorian/$year_to_fetch/$month_to_fetch/$i/ | jq '.fast_level'
+    # curl -s https://orthocal.info/api/gregorian/$year_to_fetch/$month_to_fetch/$i/ | jq '.titles' | jq '.[]'
     echo "---"
 done
--- orthoapi/orthoapi.go
+++ orthoapi/orthoapi.go
@@ -2,8 +2,12 @@ package orthoapi
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 type OrthoResponse struct {
@@ -18,21 +22,95 @@ type OrthoResponse struct {
 	FeastLevelDescription string   `json:"feast_level_desc"`
 }
 
-func MakeRequest(url string) (OrthoResponse, error) {
+const DEFAULT_URL string = "https://orthocal.info/api/gregorian/"
+const JULIAN_URL string = "https://orthocal.info/api/julian/"
+const HIDDEN_DIR_NAME string = "orthocli-"
+
+func GetFullResponse(calendarType string) (OrthoResponse, error) {
+
 	var orthoResponse OrthoResponse
-	apiResponse, err := http.Get(url)
+
+	// Find the temp dir or create it
+
+	// Read from the proper file and load it into a struct
+
+	// TODO: move all of this logic to the function meant to pull the entire year
+	apiResponse, err := makeApiRequest(DEFAULT_URL)
 
 	if err != nil {
 		return orthoResponse, err
+	}
+
+	json.Unmarshal(apiResponse, &orthoResponse)
+
+	dirName, err := createTempDir()
+	if err != nil {
+		fmt.Println("Failed to create dir: ", err)
+	}
+	fmt.Printf("Created dir: %s", dirName)
+
+	return orthoResponse, nil
+
+}
+
+func makeApiRequest(url string) ([]byte, error) {
+	apiResponse, err := http.Get(url)
+
+	if err != nil {
+		return nil, err
 	}
 
 	responseData, err := io.ReadAll(apiResponse.Body)
 
+	return responseData, err
+
+	// json.Unmarshal(responseData, &orthoResponse)
+
+	// return orthoResponse, nil
+}
+
+func createTempDir() (string, error) {
+
+	dirName, err := os.MkdirTemp("", HIDDEN_DIR_NAME)
 	if err != nil {
-		return orthoResponse, err
+		return "", err
 	}
 
-	json.Unmarshal(responseData, &orthoResponse)
+	return dirName, err
 
-	return orthoResponse, nil
 }
+
+func findTempDir(prefix string) (string, error) {
+	// Get the temporary directory location
+	tempDir := os.TempDir()
+
+	// Read the contents of the temporary directory
+	files, err := os.ReadDir(tempDir)
+	if err != nil {
+		return "", fmt.Errorf("error reading temp directory: %w", err)
+	}
+
+	// Search for the directory with the specified prefix
+	for _, file := range files {
+		if file.IsDir() && strings.HasPrefix(file.Name(), prefix) {
+			return filepath.Join(tempDir, file.Name()), nil // Return found directory path
+		}
+	}
+
+	return "", fmt.Errorf("no temporary directory found with prefix: %s", prefix)
+}
+
+// func createHiddenDir() error {
+// 	dirName := ""
+// 	if !strings.HasPrefix(HIDDEN_DIR_NAME, ".") {
+// 		dirName = "." + HIDDEN_DIR_NAME
+// 	}
+
+// 	err := os.Mkdir(dirName, 0755)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+
+// }
